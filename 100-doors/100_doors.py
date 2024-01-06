1f5b8dfcabee3039ba25f2ce245f8c45bbd748dc
foxinboxxxx <visagaboy@gmail.com>
2024-01-06 14:29:46 +0100

    Improve solution by halving

--- 100-doors/100_doors.py
+++ 100-doors/100_doors.py
@@ -3,7 +3,7 @@ import math
 
 
 """
-Initialize an array of 101 doors where 
+Initialize an array of 101 and 10001 doors where 
 False - door is closed,
 True - is opened
 """
@@ -20,9 +20,15 @@ class Doors:
                 divisors_list.append(i)
         return divisors_list
     
-    def get_open_doors(self, doors):
-        for door_number in range(1, len(doors)):
-            list_of_divisors = len(self.get_all_divisors(door_number))
+    def get_all_divisors_generator(self, number):
+        for i in range(1, int(number / 2) + 1):
+            if number % i == 0:
+                yield i
+        yield number
+
+    def get_open_doors(self, wrap_function):
+        for door_number in range(1, len(self.doors)):
+            list_of_divisors = len(list(wrap_function(door_number)))
             if list_of_divisors % 2 != 0:
                 self.list_open_doors.append(door_number)
         return self.list_open_doors
@@ -31,8 +37,21 @@ class Doors:
 if __name__ == "__main__":
     doors = Doors()
 
+    print("Improved solution, loop through half of divisors - doors.get_all_divisors_generator")
     start_time = time.process_time()
-    print(doors.get_open_doors(doors.doors))
+    print(doors.get_open_doors(doors.get_all_divisors_generator))
+    end_time = time.process_time()
+    result = end_time - start_time
+    print("Execution time: {:.7f} ms".format(result))
+    print("------------------------------\n")
+
+
+    doors = Doors()
+
+    print("Simplest solution, loop through all divisors - doors.get_all_divisors")
+    start_time = time.process_time()
+    print(doors.get_open_doors(doors.get_all_divisors))
     end_time = time.process_time()
     result = end_time - start_time
     print(f"Execution time: {result} ms")
+    print("------------------------------\n")
