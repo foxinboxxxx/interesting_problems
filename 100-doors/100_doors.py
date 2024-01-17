37558c96a4d4e4cd20167aad8e905fe7085ec4c9
foxinboxxxx <visagaboy@gmail.com>
2024-01-17 21:49:12 +0100

    Add solution with prime factor delimeter

--- 100-doors/100_doors.py
+++ 100-doors/100_doors.py
@@ -1,12 +1,14 @@
 import time
 import math
-
+import collections
+import itertools
 
 """
 Initialize an array of 101 and 10001 doors where 
 False - door is closed,
 True - is opened
 """
+
 class Doors:
     def __init__(self):
         self.doors = [False] * 101 # ignore index 0
@@ -26,17 +28,54 @@ class Doors:
                 yield i
         yield number
 
+    def get_all_divisors_prime_factor(self, number):
+        primes = self._prime_factor(number)
+        primes_counted = collections.Counter(primes)
+
+        divisors_exponentiated = [
+            [divisor ** i for i in range(count + 1)]
+            for divisor, count in primes_counted.items()
+        ]
+
+        for prime_exp_combination in itertools.product(*divisors_exponentiated):
+            yield self._calculate_product(prime_exp_combination)
+
+    def _prime_factor(self, number):
+        delimeter_count = 2
+        while math.sqrt(delimeter_count) <= number:
+            if number % delimeter_count == 0:
+                number /= delimeter_count
+                yield delimeter_count
+            else:
+                delimeter_count += 1
+
+        if number > 1:
+            yield number
+    
+    def _calculate_product(self, iterable):
+        acc = 1
+        for i in iterable:
+            acc *= i
+        return acc
+
     def get_open_doors(self, wrap_function):
-        for door_number in range(1, len(self.doors)):
+        for door_number in range(1, len(self.doors_10000)):
             list_of_divisors = len(list(wrap_function(door_number)))
             if list_of_divisors % 2 != 0:
                 self.list_open_doors.append(door_number)
         return self.list_open_doors
 
-
 if __name__ == "__main__":
     doors = Doors()
+    print("Simplest solution, loop through all divisors - doors.get_all_divisors")
+    start_time = time.process_time()
+    print(doors.get_open_doors(doors.get_all_divisors))
+    end_time = time.process_time()
+    result = end_time - start_time
+    print(f"Execution time: {result} ms")
+    print("------------------------------\n")
 
+    doors = Doors()
     print("Improved solution, loop through half of divisors - doors.get_all_divisors_generator")
     start_time = time.process_time()
     print(doors.get_open_doors(doors.get_all_divisors_generator))
@@ -45,12 +84,10 @@ if __name__ == "__main__":
     print("Execution time: {:.7f} ms".format(result))
     print("------------------------------\n")
 
-
     doors = Doors()
-
-    print("Simplest solution, loop through all divisors - doors.get_all_divisors")
+    print("Prime factor solution - doors.prime_factor")
     start_time = time.process_time()
-    print(doors.get_open_doors(doors.get_all_divisors))
+    print(doors.get_open_doors(doors.get_all_divisors_prime_factor))
     end_time = time.process_time()
     result = end_time - start_time
     print(f"Execution time: {result} ms")
